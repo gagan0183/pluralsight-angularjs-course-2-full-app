e57710dc87b1f3872de082814dd4b0703819d6ba
gagan0183 <g.deepsingh1@gmail.com>
2017-06-17 15:20:28 +0530

    complete validations p

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -1,6 +1,8 @@
 eventsApp.controller('EditEventController', function($scope) {
-    $scope.saveEvent = function(event) {
-        
+    $scope.saveEvent = function(event, newEventForm) {
+        if(newEventForm.$valid) {
+            window.alert("this");
+        }
     };
 
     $scope.cancelEdit = function() {
